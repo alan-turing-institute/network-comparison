be473b68edb951fb0164705c60a200e9e94c10e9
Martin O'Reilly <developer@mtspace.net>
2016-12-23 00:14:13 +0000

    Added EMD method using the difference of cumulative sums method from Vallender 1974

--- R/measures-net-emd.R
+++ R/measures-net-emd.R
@@ -1,58 +1,130 @@
 library("lpSolve")
 
 #' Earth Mover's Distance (EMD) using linear programming (LP)
-#' Takes two sets of feature counts and locations and calculates the 
-#' Earth Mover's Distance between them. EMD is also known as the Wasserstein 
-#' distance or metric
-#' Required parameters are the counts of each feature for both networks
-#' @param bin_counts1 Feature counts for network 1
-#' @param bin_counts2 Feature counts for network 2
-#' Optional parameters are the locations of each feature. If omitted, it is 
-#' assumed that features are separated by unit distance
-#' @param bin_locations1 Feature bin locations for network 1
-#' @param bin_locations2 Feature bin locations for network 2
-emd_lp <- function(bin_counts1, bin_counts2, bin_locations1, bin_locations2) {
+#' 
+#' Takes two sets of histogram bin masses and bin centres and calculates the 
+#' Earth Mover's Distance between the two histograms by solving the Transport
+#' Problem using linear programming.
+#' @param bin_masses1 Bin masses for histogram 1
+#' @param bin_masses2 Bin masses for histogram 2
+#' @param bin_centres1 Bin centres for histogram 1
+#' @param bin_centres2 Bin centres for histogram 2
+#' @export
+emd_lp <- function(bin_masses1, bin_masses2, bin_centres1, bin_centres2) {
   
-  num_bins1 <- length(bin_counts1)
-  if(missing(bin_locations1)) {
-    bin_locations1 <- 1:num_bins1
-  }
-  num_locations1 <- length(bin_locations1)
-  num_bins2 <- length(bin_counts2)
-  if(missing(bin_locations2)) {
-    bin_locations2 <- 1:num_bins2
-  }
-  num_locations2 <- length(bin_locations2)
+  num_bins1 <- length(bin_masses1)
+  num_bins2 <- length(bin_masses2)
   
   # Check inputs. Size of bin counts and locations must match for each histogram
   # but the histograms for the two networks can have different numbers of bins
   # as only non-zero bins are required
-  if(num_bins1 != num_locations1) {
-    stop("bin_counts1 and bin_locations1 must be of equal length")
+  if(length(bin_centres1) != num_bins1) {
+    stop("Number of bin masses and bin centres provided for histogram 1 must be equal")
   }
-  if(num_bins2 != num_locations2) {
-    stop("bin_counts2 and bin_locations2 must be of equal length")
+  if(length(bin_centres2) != num_bins2) {
+    stop("Number of bin masses and bin centres provided for histogram 2 must be equal")
   }
   
-  cost_mat <- cost_matrix(bin_locations1, bin_locations2)
+  cost_mat <- cost_matrix(bin_centres1, bin_centres2)
   row_signs <- rep("==", num_bins1)
   col_signs <- rep("<=", num_bins2)
   s <- lpSolve::lp.transport(cost.mat = cost_mat, row.signs = row_signs, 
-                    col.signs = col_signs, row.rhs = bin_counts1, 
-                    col.rhs = bin_counts2)
+                    col.signs = col_signs, row.rhs = bin_masses1, 
+                    col.rhs = bin_masses2)
   return(s$objval)
 }
 
-#' Generates a matrix for the cost of moving a unit of mass between all pairs of
-#' features. The cost is the distance between the locations of each feature pair
-#' @param bin_locations1 Feature bin locations for network 1
-#' @param bin_locations2 Feature bin locations for network 2 
-cost_matrix <- function(bin_locations1, bin_locations2) {
+#' Earth Mover's Distance (EMD) using the difference of cumulative sums method
+#' 
+#' Takes two sets of histogram bin masses and bin centres and calculates the 
+#' Earth Mover's Distance between the two histograms by summing the absolute 
+#' difference between the two cumulative histograms.
+#' @references 
+#' Calculation of the Wasserstein Distance Between Probability Distributions on the Line
+#' S. S. Vallender, Theory of Probability & Its Applications 1974 18:4, 784-786
+#' \url{http://dx.doi.org/10.1137/1118101}
+#' @param bin_masses1 Bin masses for histogram 1
+#' @param bin_masses2 Bin masses for histogram 2
+#' @param bin_centres1 Bin centres for histogram 1
+#' @param bin_centres2 Bin centres for histogram 2
+#' @export
+emd_cs <- function(bin_masses1, bin_masses2, bin_centres1, bin_centres2) {
+  
+  # Ensure both histograms have entries for all bins that appear in either histogram
+  ah <- augment_histograms(bin_masses1, bin_masses2, bin_centres1, bin_centres2)
+  bin_masses1 <- ah$bin_masses1
+  bin_masses2 <- ah$bin_masses2
+  bin_centres1 <- ah$bin_centres1
+  bin_centres2 <- ah$bin_centres2
+  
+  # Sort bin centres and masses in bin centre order
+  sorted_indexes1 <- sort(bin_centres1, decreasing = FALSE, index.return = TRUE)$ix
+  sorted_indexes2 <- sort(bin_centres2, decreasing = FALSE, index.return = TRUE)$ix
+  sorted_masses1 <- bin_masses1[sorted_indexes1]
+  sorted_masses2 <- bin_masses2[sorted_indexes2]
+  sorted_centres1 <- bin_centres1[sorted_indexes1]
+  sorted_centres2 <- bin_centres2[sorted_indexes2]
+  
+  # Generate cumulative histogram masses
+  cum_mass1 = cumsum(sorted_masses1)
+  cum_mass2 <- cumsum(sorted_masses2)
+  
+  # Determine spacing between bin centres
+  vector_spacing <- function(v) {
+    tail(v, length(v)-1) - head(v, length(v)-1)
+  }
+  sorted_centre_spacing <- vector_spacing(sorted_centres1)
+  
+  # Discrete integration of absolute difference between cumulative histograms
+  # - Get difference between cumulative histograms at each bin with data
+  cum_mass_diff_at_bins <- apply(rbind(cum_mass1, cum_mass2), 2, function(x) abs(x[1] - x[2]))
+  # - Multiply the difference at each bin by the distance to the next bin to get
+  # - the area under the difference curve between consecutive bins (bins are 
+  # - not guaranteed to be equally sized or equally spaced and bins with no 
+  # - mass in either histogram may not be present at all)
+  cum_mass_diff_area_between_bins <- 
+    head(cum_mass_diff_at_bins, length(cum_mass_diff_at_bins) - 1) * 
+    sorted_centre_spacing
+  
+  return(sum(cum_mass_diff_area_between_bins))
+}
+
+#' Inter-bin cost matrix from bin centres
+#' 
+#' Generates a matrix for the cost of moving a unit of mass between each bin in 
+#' histogram 1 and each bin in histogram 2.
+#' @param bin_centres1 Bin centres for histogram 1
+#' @param bin_centres2 Bin centres for histogram 2
+cost_matrix <- function(bin_centres1, bin_centres2) {
   # Calculate distances between all bins in network 1 and all bins in network 2
-  num_bins1 <- length(bin_locations1) 
-  num_bins2 <- length(bin_locations2)
-  loc_mat1 <- matrix(bin_locations1, nrow = num_bins1, ncol = num_bins2, byrow = FALSE)
-  loc_mat2 <- matrix(bin_locations2, nrow = num_bins1, ncol = num_bins2, byrow = TRUE)
+  num_bins1 <- length(bin_centres1) 
+  num_bins2 <- length(bin_centres2)
+  loc_mat1 <- matrix(bin_centres1, nrow = num_bins1, ncol = num_bins2, byrow = FALSE)
+  loc_mat2 <- matrix(bin_centres2, nrow = num_bins1, ncol = num_bins2, byrow = TRUE)
   cost_mat <- abs(loc_mat1 - loc_mat2)
   return(cost_mat)
+}
+
+#' Augment a pair of histograms to share a common set of bins
+#' 
+#' Where a bin centre only exists in one histogram, add a zero mass bin with the
+#' same centre to the other histogram to ensure that all bins exist in both 
+#' histograms. This makes some hsitogram operations easier.
+#' @param bin_masses1 Bin masses for histogram 1
+#' @param bin_masses2 Bin masses for histogram 2
+#' @param bin_centres1 Bin centres for histogram 1
+#' @param bin_centres2 Bin centres for histogram 2
+augment_histograms <- function(bin_masses1, bin_masses2, bin_centres1, bin_centres2) {
+  # Identify missing bin centres in each histogram
+  missing_centres1 <- setdiff(bin_centres2, bin_centres1)
+  missing_centres2 <- setdiff(bin_centres1, bin_centres2)
+  # Add missing centres to end of each histogram
+  bin_centres1 <- c(bin_centres1, missing_centres1)
+  bin_centres2 <- c(bin_centres2, missing_centres2)
+  # Assign these extra bins zero mass
+  bin_masses1 <- c(bin_masses1, rep(0, length(missing_centres1)))
+  bin_masses2 <- c(bin_masses2, rep(0, length(missing_centres2)))
+  
+  list(bin_masses1 = bin_masses1, bin_masses2 = bin_masses2, 
+       bin_centres1 = bin_centres1, bin_centres2 = bin_centres2)
 }
--- tests/testthat/test-measures-net-emd.R
+++ tests/testthat/test-measures-net-emd.R
@@ -3,67 +3,187 @@ context("NetEMD")
 
 # COST_MATRIX: Property-based tests
 test_that("cost_matrix returns all zeros when all bin locations are identical", {
-  bin_locations1 <- c(1, 1, 1, 1, 1, 1, 1)
-  bin_locations2 <- bin_locations1
-  expected <- matrix(0, nrow = length(bin_locations1), ncol = length(bin_locations2))
-  expect_equal(cost_matrix(bin_locations1, bin_locations2), expected)
+  bin_centres1 <- c(1, 1, 1, 1, 1, 1, 1)
+  bin_centres2 <- bin_centres1
+  expected <- matrix(0, nrow = length(bin_centres1), ncol = length(bin_centres2))
+  expect_equal(cost_matrix(bin_centres1, bin_centres2), expected)
 })
 
 test_that("cost_matrix returns zeros along diagonal when both sets of bin locations are the same", {
-  bin_locations1 <- c(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5)
-  bin_locations2 <- bin_locations1
-  expected <- rep(0, length(bin_locations1))
-  expect_equal(diag(cost_matrix(bin_locations1, bin_locations2)), expected)
+  bin_centres1 <- c(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5)
+  bin_centres2 <- bin_centres1
+  expected <- rep(0, length(bin_centres1))
+  expect_equal(diag(cost_matrix(bin_centres1, bin_centres2)), expected)
 })
 
 test_that("cost_matrix returns zeros along diagonal and taxicab distance from all zeros for all other elements when both sets of bin locations are the same and are a sequence of consecutive integers", {
-  bin_locations1 <- c(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5)
-  bin_locations2 <- bin_locations1
-  num_bins <- length(bin_locations1)
+  bin_centres1 <- c(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5)
+  bin_centres2 <- bin_centres1
+  num_bins <- length(bin_centres1)
   expected <- toeplitz(1:num_bins)-1
-  expect_equal(cost_matrix(bin_locations1, bin_locations2), expected)
+  expect_equal(cost_matrix(bin_centres1, bin_centres2), expected)
 })
 
-# EMD_LP: Property-based tests
-test_that("emd_lp returns 0 when comparing a 1D feature distribution to itself (implicit unit interval locations)",{
-  bin_counts1 <- c(0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0)
-  bin_counts2 <- bin_counts1
-  expect_equal(emd_lp(bin_counts1, bin_counts2), 0)
+test_that("cost_matrix is correct size when the two histograms are of different lengths", {
+  bin_centres1 <- c(1, 2, 3, 4, 5, 6, 7)
+  bin_centres2 <- c(8, 9, 10)
+  
+  cm <- cost_matrix(bin_centres1, bin_centres2)
+  
+  expect_equal(nrow(cm), length(bin_centres1))
+  expect_equal(ncol(cm), length(bin_centres2))
 })
 
-test_that("emd_lp returns 0 when comparing a 1D feature distribution to itself (explicit locations)",{
-  bin_counts1 <- c(0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0)
-  bin_counts2 <- bin_counts1
-  bin_locations1 <- c(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5)
-  bin_locations2 <- bin_locations1
-  expect_equal(emd_lp(bin_counts1, bin_counts2, bin_locations1, bin_locations2), 0)
+# AUGMENT_HISTOGRAMS
+test_that("augment_histograms works", {
+  bin_masses1 <- c(1, 1, 1)
+  bin_masses2 <- c(1, 1, 1)
+  bin_centres1 <- c(1, 3, 5)
+  bin_centres2 <- c(2, 4, 6)
+  
+  expected <- list(
+    bin_masses1 = c(1, 1, 1, 0, 0, 0),
+    bin_masses2 = c(1, 1, 1, 0, 0, 0),
+    bin_centres1 = c(1, 3, 5, 2, 4, 6),
+    bin_centres2 = c(2, 4, 6, 1, 3, 5)
+  )
+  actual <- augment_histograms(bin_masses1, bin_masses2, bin_centres1, bin_centres2)
+  expect_equal(actual, expected)
+  
+})
+test_that("augment_histograms works", {
+  bin_masses1 <- c(1, 1, 1)
+  bin_masses2 <- c(1, 1, 1)
+  bin_centres1 <- c(1, 3, 5)
+  bin_centres2 <- c(4, 5, 6)
+  
+  expected <- list(
+    bin_masses1 = c(1, 1, 1, 0, 0),
+    bin_masses2 = c(1, 1, 1, 0, 0),
+    bin_centres1 = c(1, 3, 5, 4, 6),
+    bin_centres2 = c(4, 5, 6, 1, 3)
+  )
+  actual <- augment_histograms(bin_masses1, bin_masses2, bin_centres1, bin_centres2)
+  expect_equal(actual, expected)
 })
 
-test_that("emd_lp returns numBins/2 when offsetting a symmetric discrete triangle distribution by 1 (implicit unit interal locations)", {
+# EMD_LP and EMD_CS: Property-based tests
+test_that("EMD methods return 0 when comparing a 1D feature distribution to itself",{
+  bin_masses1 <- c(0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0)
+  bin_masses2 <- bin_masses1
+  bin_centres1 <- c(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5)
+  bin_centres2 <- bin_centres1
+  
+  expected <- 0
+  expect_equal(emd_lp(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
+  expect_equal(emd_cs(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
+})
+
+test_that("EMD methods return numBins/2 when offsetting a symmetric discrete triangle distribution by 1", {
   cost_fn <- function(triangle_width) {
     move_dist <- ceiling((triangle_width+1)/2)
     num_moves <- ceiling(triangle_width/2)
     return(move_dist * num_moves)
   }
   
-  bin_counts1 <- c(0, 1, 2, 3, 4, 4, 3, 2, 1, 0)
-  bin_counts2 <- c(0, 0, 1, 2, 3, 4, 4, 3, 2, 1)
-  expected = cost_fn(sum(bin_counts1>0))
-  expect_equal(emd_lp(bin_counts1, bin_counts2), expected)
+  bin_masses1 <- c(0, 1, 2, 3, 4, 4, 3, 2, 1, 0)
+  bin_masses2 <- c(0, 0, 1, 2, 3, 4, 4, 3, 2, 1)
+  bin_centres1 <- 1:length(bin_masses1)
+  bin_centres2 <- 1:length(bin_masses2)
+  num_nonzero_bins <- sum(bin_masses1 > 0)
+  expected <- cost_fn(num_nonzero_bins)
+  expect_equal(emd_lp(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
+  expect_equal(emd_cs(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
   
-  bin_counts1 <- c(0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0)
-  bin_counts2 <- c(0, 0, 1, 2, 3, 4, 5, 4, 3, 2, 1)
-  expected = cost_fn(sum(bin_counts1>0))
-  expect_equal(emd_lp(bin_counts1, bin_counts2), expected)
+  bin_masses1 <- c(0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0)
+  bin_masses2 <- c(0, 0, 1, 2, 3, 4, 5, 4, 3, 2, 1)
+  bin_centres1 <- 1:length(bin_masses1)
+  bin_centres2 <- 1:length(bin_masses2)
+  expected <- cost_fn(sum(bin_masses1 > 0))
+  expect_equal(emd_lp(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
+  expect_equal(emd_cs(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
   
-  bin_counts1 <- c(0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 0)
-  bin_counts2 <- c(0, 0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1)
-  expected = cost_fn(sum(bin_counts1>0))
-  expect_equal(emd_lp(bin_counts1, bin_counts2), expected)
+  bin_masses1 <- c(0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 0)
+  bin_masses2 <- c(0, 0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1)
+  bin_centres1 <- 1:length(bin_masses1)
+  bin_centres2 <- 1:length(bin_masses2)
+  expected <- cost_fn(sum(bin_masses1 > 0))
+  expect_equal(emd_lp(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
+  expect_equal(emd_cs(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
   
-  bin_counts1 <- c(0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0)
-  bin_counts2 <- c(0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1)
-  expected = cost_fn(sum(bin_counts1>0))
-  expect_equal(emd_lp(bin_counts1, bin_counts2), expected)
+  bin_masses1 <- c(0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0)
+  bin_masses2 <- c(0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1)
+  bin_centres1 <- 1:length(bin_masses1)
+  bin_centres2 <- 1:length(bin_masses2)
+  expected <- cost_fn(sum(bin_masses1 > 0))
+  expect_equal(emd_lp(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
+  expect_equal(emd_cs(bin_masses1, bin_masses2, bin_centres1, bin_centres2), expected)
   
 })
+
+test_that("EMD methods return same result for densely and sparsely specified bins", {
+  sparse_bin_masses1 <- c(1, 1, 1, 1, 1, 1)
+  sparse_bin_masses2 <- c(1, 1, 1, 1, 1, 1)
+  sparse_bin_centres1 <- c(1, 2, 4, 7, 11, 16)
+  sparse_bin_centres2 <- c(21, 22, 24, 27, 31, 36)
+  
+  dense_bin_centres1 <- 1:36
+  dense_bin_centres2 <- dense_bin_centres1
+  bin_mass_sequence <- c(1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1)
+  bin_mass_padding <- rep(0, length(dense_bin_centres1) - length(bin_mass_sequence))
+  dense_bin_masses1 <- c(bin_mass_sequence, bin_mass_padding)
+  dense_bin_masses2 <- c(bin_mass_padding, bin_mass_sequence)
+  
+  expect_equal(emd_lp(dense_bin_masses1, dense_bin_masses2, 
+                    dense_bin_centres1, dense_bin_centres2),
+              emd_lp(sparse_bin_masses1, sparse_bin_masses2, 
+                    sparse_bin_centres1, sparse_bin_centres2))
+  expect_equal(emd_cs(dense_bin_masses1, dense_bin_masses2, 
+                    dense_bin_centres1, dense_bin_centres2),
+              emd_cs(sparse_bin_masses1, sparse_bin_masses2, 
+                    sparse_bin_centres1, sparse_bin_centres2))
+})
+
+test_that("EMD methods return same result when order of densely specified bins is changed", {
+  bin_masses1 <- c(1, 1, 1, 1, 0, 0, 0, 0, 0)
+  bin_masses2 <- c(0, 0, 0, 0, 0, 1, 1, 1, 1)
+  bin_centres1 <- 1:length(bin_masses1)
+  bin_centres2 <- 1:length(bin_masses2)
+  
+  permuted_indexes1 <- sample(1:length(bin_masses1), replace = FALSE)
+  permuted_indexes2 <- sample(1:length(bin_masses2), replace = FALSE)
+  
+  permuted_bin_masses1 <- bin_masses1[permuted_indexes1]
+  permuted_bin_centres1 <- bin_centres1[permuted_indexes1]
+  permuted_bin_masses2 <- bin_masses2[permuted_indexes2]
+  permuted_bin_centres2 <- bin_centres2[permuted_indexes2]
+  
+  expect_equal(emd_lp(bin_masses1, bin_masses2, bin_centres1, bin_centres2),
+              emd_lp(permuted_bin_masses1, permuted_bin_masses2, 
+                    permuted_bin_centres1, permuted_bin_centres2))
+  expect_equal(emd_cs(bin_masses1, bin_masses2, bin_centres1, bin_centres2),
+              emd_cs(permuted_bin_masses1, permuted_bin_masses2, 
+                    permuted_bin_centres1, permuted_bin_centres2))
+})
+
+test_that("EMD methods return same result when order of sparsely specified bins is changed", {
+  bin_masses1 <- c(1, 1, 1, 1, 1, 1)
+  bin_masses2 <- c(1, 1, 1, 1, 1, 1)
+  bin_centres1 <- c(1, 2, 4, 8, 16, 32)
+  bin_centres2 <- c(-32, -16, -8, -4, -2, -1)
+  
+  permuted_indexes1 <- sample(1:length(bin_masses1), replace = FALSE)
+  permuted_indexes2 <- sample(1:length(bin_masses2), replace = FALSE)
+  
+  permuted_bin_masses1 <- bin_masses1[permuted_indexes1]
+  permuted_bin_centres1 <- bin_centres1[permuted_indexes1]
+  permuted_bin_masses2 <- bin_masses2[permuted_indexes2]
+  permuted_bin_centres2 <- bin_centres2[permuted_indexes2]
+  
+  expect_equal(emd_lp(bin_masses1, bin_masses2, bin_centres1, bin_centres2),
+               emd_lp(permuted_bin_masses1, permuted_bin_masses2, 
+                      permuted_bin_centres1, permuted_bin_centres2))
+  expect_equal(emd_cs(bin_masses1, bin_masses2, bin_centres1, bin_centres2),
+               emd_cs(permuted_bin_masses1, permuted_bin_masses2, 
+                      permuted_bin_centres1, permuted_bin_centres2))
+  })
