d1a89c31dbedc94660aebe45c1eccea4a77ce8e2
Martin O'Reilly <developer@mtspace.net>
2017-04-07 01:53:01 +0100

    Fix package-relative paths to work with R CMD check
    Was using system.file(file.path(“inst”, ”extdata", …), package = "netdist") to read
    from raw data files in tests. This worked when testing with devotees::test() but
    failed when testing with devotees::check(). This appears to be due to the
    fact that the files in “inst” are moved to the base directory of the package during
    installation. I’m not sure why devtools::test() works both with or without the “inst”
    in the relative path from the package root.

--- tests/testthat/test_orca_interface.R
+++ tests/testthat/test_orca_interface.R
@@ -1,10 +1,10 @@
 library("igraph")
-data_dir <- system.file(file.path("inst", "extdata", "VRPINS"), package = "netdist")
+data_dir <- system.file(file.path("extdata", "VRPINS"), package = "netdist")
 
 context("ORCA interface: Graph to indexed edge round trip")
 test_that("Graph to indexed edge list round trip conversion works", {
   g_orig <- igraph::read_graph(file = file.path(data_dir, "EBV-1.txt"), format = "ncol")
-  g_rtrip <- indexed_edges_to_graph(graph_to_indexed_edges(g_orig))
+  g_rtrip <- netdist::indexed_edges_to_graph(graph_to_indexed_edges(g_orig))
   expect_true(all.equal(igraph::get.edgelist(g_orig),igraph::get.edgelist(g_orig)))
 })
 
