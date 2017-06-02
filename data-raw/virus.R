d8edc56cec2829f394a0ccd4b8191bc8cbd0f94c
Martin O'Reilly <developer@mtspace.net>
2017-06-02 12:25:43 +0100

    Update embedded virusppi data and generator script
    Updated to match changes to filename for HSV-1 data

--- data-raw/virus.R
+++ data-raw/virus.R
@@ -7,11 +7,11 @@ load_virus_data <- function(filename) {
   read_orca_graph(file = file.path(data_dir, filename), format = "ncol")
 }
 
-virusppi <- list(EBV = load_virus_data("EBV-1.txt"), 
-                 ECL = load_virus_data("ECL-1.txt"),
-                 HSV = load_virus_data("HSV-1-1.txt"),
-                 KSHV = load_virus_data("KSHV-1.txt"),
-                 VZV = load_virus_data("VZV-1.txt")
+virusppi <- list(EBV = load_virus_data("EBV.txt"), 
+                 ECL = load_virus_data("ECL.txt"),
+                 `HSV-1` = load_virus_data("HSV-1.txt"),
+                 KSHV = load_virus_data("KSHV.txt"),
+                 VZV = load_virus_data("VZV.txt")
                  )
 
 devtools::use_data(virusppi, overwrite = TRUE)
