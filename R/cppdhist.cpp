41170f334f4c6d6d7fb6c2ac000ddff445fce7d4
andeElliott <ande.elliott@gmail.com>
2017-11-14 10:53:34 +0000

    fixed bugs in the implementation, seems to be much faster

--- R/cppdhist.cpp
+++ R/cppdhist.cpp
@@ -18,41 +18,42 @@ double constantVersion(NumericVector loc1,NumericVector val1,NumericVector loc2,
    double res=0;
    double curVal1,curVal2;
    double curPos;
+   double temp1;
+   int count;
    int i,j,k;
-   int maxLen;
-   maxLen=std::max(loc1.size(),loc2.size());
    if (loc1[0]<loc2[0])
    {
-       res+=(loc2[0]-loc1[0])*(val1[0]);
-       curPos=loc2[0];
-       curVal1=val1[0];
-       curVal2=val2[0];
+       curPos=loc1[0]-1.0;
    }
    else
    {
-       res+=(loc1[0]-loc2[0])*(val2[0]);
-       curPos=loc1[0];
-       curVal1=val1[0];
-       curVal2=val2[0];
+       curPos=loc2[0]-1.0;
    }
-   i=1;
-   j=1;
-   for (k=0;k<=maxLen;k++)
+   curVal1=0;
+   curVal2=0;
+   res=0;
+   i=0;
+   j=0;
+    count=0;
+       while (1)
     {
+        count+=1;
         if (i==loc1.size())
         {break;}
         if (j==loc2.size())
         {break;}
         if (loc1[i]<loc2[j])
         {
-            res+=(loc1[i]-curPos)*abs(curVal1-curVal2);
+            temp1=(loc1[i]-curPos)*abs(curVal1-curVal2);
+            res+=temp1;
             curVal1=val1[i];
             curPos=loc1[i];
             i+=1;
         }
         else
         {
-            res+=(loc2[j]-curPos)*abs(curVal1-curVal2);
+            temp1=(loc2[j]-curPos)*abs(curVal1-curVal2);
+            res+=temp1;
             curVal2=val2[j];
             curPos=loc2[j];
             j+=1;
@@ -79,47 +80,3 @@ double constantVersion(NumericVector loc1,NumericVector val1,NumericVector loc2,
     return res;
 }
 
-
-//#double func(loc1,val1,loc2,val2)
-//#//We can assume that the locations are sorted
-//#//therefore we just need to merge them
-//#   std:vector<double> x;
-//#   std:vector<double> val1;
-//#   std:vector<double> val2;
-//#   int i,j,k;
-//#   int maxLen;
-//#   maxLen=max(loc1.size(),loc2.size())
-//#    i=0;
-//#    j=0;
-//#   for (k=0;k<maxLen;k++)
-//#    {
-//#        if (i==loc1.size())
-//#        {break;}
-//#        if (j==loc2.size())
-//#        {break;}
-//#        if (loc1[i]<loc2[j])
-//#        {
-//#            x.push_back(loc1[i]);
-//#            i+=1;
-//#        }
-//#        else
-//#        {
-//#            x.push_back(loc1[j]);
-//#            j+=1;
-//#        }
-//#    }
-//#    if (i<loc1.size())
-//#    {
-//#        for (k=i;k<loc1.size();k++)
-//#        {
-//#            x.push_back(loc1[k]);
-//#        }
-//#    }
-//#    else
-//#    {
-//#        for (k=j;k<loc2.size();k++)
-//#        {
-//#            x.push_back(loc2[k]);
-//#        }
-//#    }
-//#
