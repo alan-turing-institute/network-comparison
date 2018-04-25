051227882a721f5f0a690626ebc5c2b94d51f01d
ande <ande.elliott@gmail.com>
2018-04-25 17:33:27 +0100

    editted the files for the build process
    copied some of the code into the median process file.
    
    Decided not to fight the R build process

--- src/medianApproach.cpp
+++ src/medianApproach.cpp
@@ -5,10 +5,86 @@
 #include <algorithm>
 #include <numeric>
 #include <math.h>
-#include "src/fastNoSmooth.hpp"
+//#include "src/fastNoSmooth.hpp"
 #include <cstdio>
 #include <ctime>
 using namespace Rcpp;
+
+
+
+double NetEmdConstant(NumericVector loc1,NumericVector val1,NumericVector loc2,NumericVector val2)
+{
+  //init
+   double res=0;
+   double curVal1,curVal2;
+   double curPos;
+   double temp1;
+   int count;
+   int i,j,k;
+   //place start of windows before
+   //start of histogram so we can start the loop
+   if (loc1[0]<loc2[0])
+   {
+       curPos=loc1[0]-1.0;
+   }
+   else
+   {
+       curPos=loc2[0]-1.0;
+   }
+   // current value of histogram 1 an 2
+   curVal1=0;
+   curVal2=0;
+   // stores the result
+   res=0;
+   //TODO be worried about adding lots of small numbers
+
+   // current location on hist 1 and hist 2
+   i=0;
+   j=0;
+    while (1)
+    {
+        if (i==loc1.size())
+        {break;}
+        if (j==loc2.size())
+        {break;}
+        if (loc1[i]<loc2[j])
+        {
+            temp1=(loc1[i]-curPos)*std::abs(curVal1-curVal2);
+            res+=temp1;
+            curVal1=val1[i];
+            curPos=loc1[i];
+            i+=1;
+        }
+        else
+        {
+            temp1=(loc2[j]-curPos)*std::abs(curVal1-curVal2);
+            res+=temp1;
+            curVal2=val2[j];
+            curPos=loc2[j];
+            j+=1;
+        }
+    }
+    if (i<loc1.size())
+    {
+        for (k=i;k<loc1.size();k++)
+        {
+            res+=(loc1[k]-curPos)*(1.0-curVal1);
+            curVal1=val1[k];
+            curPos=loc1[k];
+        }
+    }
+    else
+    {
+        for (k=j;k<loc2.size();k++)
+        {
+            res+=(loc2[k]-curPos)*(1.0-curVal2);
+            curVal2=val2[k];
+            curPos=loc2[k];
+        }
+    }
+    return res;
+}
+
 
 bool pairComp(std::pair<double,double> v1,std::pair<double,double> v2)
 {
