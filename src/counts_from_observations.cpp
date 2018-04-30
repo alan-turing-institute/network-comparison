4e9e8e01a2d5fabc4f4ff6aa44f7029ec93c0af3
Martin O'Reilly <developer@mtspace.net>
2018-04-30 16:11:13 +0100

    Add comments to counts_from_observations C++ source

--- src/counts_from_observations.cpp
+++ src/counts_from_observations.cpp
@@ -15,18 +15,24 @@ using namespace Rcpp;
 NumericMatrix  counts_from_observations(NumericMatrix features)
 {
     std::unordered_map<double,int> counter;
-    int i;
-    // Silly solution to the float comparison problem
-    for (i=0;i<features.nrow();i++) {
+    // Generate a map with one key for every unique value in the features vector
+    // and a zero count for each value. If the elements of the feature vector
+    // are floating point numbers, a new key will be created in the map unless
+    // the elements are exactly equal.
+    for (int i=0; i<features.nrow(); i++) {
       counter[features[i]]=0;
     }
-    for (i=0;i<features.nrow();i++) {
+    // Accumulate the counts for each unique element in the feature vector
+    for (int i=0;i<features.nrow();i++) {
       counter[features[i]]+=1;
     }
+    // Convert the unordered map into a vector so we can sort it by the 
+    // feature elements
     std::vector<std::pair<double,int> > keys(counter.begin(),counter.end());
     std::sort(keys.begin(),keys.end());
+    // Create an output matrix to return to R
     NumericMatrix result(keys.size(),2);
-    for (i=0;i<keys.size();i++)
+    for (int i=0;i<keys.size();i++)
     {
         result(i,0)=keys[i].first;
         result(i,1)=keys[i].second;
