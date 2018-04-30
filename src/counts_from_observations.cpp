90da6d9cb43a805dbf7bcc943e760df5bc3b8517
Martin O'Reilly <developer@mtspace.net>
2018-04-30 13:37:58 +0100

    Rename C++ counts_from_observations file and make style changes to code

--- src/counts_from_observations.cpp
+++ src/counts_from_observations.cpp
@@ -6,7 +6,7 @@ using namespace Rcpp;
 //' @title
 //' Count number of occurences
 //'
-//' @param features a Matrix with doubles.
+//' @param features A Matrix with doubles.
 //'
 //' @export
 // [[Rcpp::export]]
@@ -15,10 +15,12 @@ NumericMatrix  counts_from_observations(NumericMatrix features)
     std::unordered_map<double,int> counter;
     int i;
     // Silly solution to the float comparison problem
-    for (i=0;i<features.nrow();i++)
-    {counter[features[i]]=0;}
-    for (i=0;i<features.nrow();i++)
-    {counter[features[i]]+=1;}
+    for (i=0;i<features.nrow();i++) {
+      counter[features[i]]=0;
+    }
+    for (i=0;i<features.nrow();i++) {
+      counter[features[i]]+=1;
+    }
     std::vector<std::pair<double,int> > keys(counter.begin(),counter.end());
     std::sort(keys.begin(),keys.end());
     NumericMatrix result(keys.size(),2);
