cd9a7a60942377c28cf4cb583e9d074191b2ef8e
ande <ande.elliott@gmail.com>
2018-06-01 11:31:11 +0100

    updated auto generated file

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,7 +7,7 @@ using namespace Rcpp;
 
 // counts_from_observations
 NumericMatrix counts_from_observations(NumericMatrix features);
-RcppExport SEXP netdist_counts_from_observations(SEXP featuresSEXP) {
+RcppExport SEXP _netdist_counts_from_observations(SEXP featuresSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -15,4 +15,31 @@ BEGIN_RCPP
     rcpp_result_gen = Rcpp::wrap(counts_from_observations(features));
     return rcpp_result_gen;
 END_RCPP
+}
+// NetEmdSmooth
+double NetEmdSmooth(NumericVector loc1, NumericVector val1, double binWidth1, NumericVector loc2, NumericVector val2, double binWidth2);
+RcppExport SEXP _netdist_NetEmdSmooth(SEXP loc1SEXP, SEXP val1SEXP, SEXP binWidth1SEXP, SEXP loc2SEXP, SEXP val2SEXP, SEXP binWidth2SEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< NumericVector >::type loc1(loc1SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type val1(val1SEXP);
+    Rcpp::traits::input_parameter< double >::type binWidth1(binWidth1SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type loc2(loc2SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type val2(val2SEXP);
+    Rcpp::traits::input_parameter< double >::type binWidth2(binWidth2SEXP);
+    rcpp_result_gen = Rcpp::wrap(NetEmdSmooth(loc1, val1, binWidth1, loc2, val2, binWidth2));
+    return rcpp_result_gen;
+END_RCPP
+}
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_netdist_counts_from_observations", (DL_FUNC) &_netdist_counts_from_observations, 1},
+    {"_netdist_NetEmdSmooth", (DL_FUNC) &_netdist_NetEmdSmooth, 6},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_netdist(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }
