ceabbb5d76680f8683a1783927f41a68ac5f4ced
ande <ande.elliott@gmail.com>
2018-07-06 19:18:51 +0100

    version 0.1 of the optimised version for the paper

--- src/medianApproach.cpp
+++ src/medianApproach.cpp
@@ -271,3 +271,232 @@ NumericVector NetEmdExhaustiveMedian(NumericVector loc1,NumericVector val1,Numer
     }
 }
 
+
+//' @title
+//' Compute EMD
+////'
+////' @param loc1 numeric vector.
+////' @param val1 numeric vector.
+////' @param loc2 numeric vector.
+////' @param val2 numeric vector.
+//'
+//' @export
+// [[Rcpp::export]]
+NumericVector NetEmdExhaustiveMedianMk2(NumericVector loc1,NumericVector val1,NumericVector loc2,NumericVector val2,double lowerLimit,double upperLimit)
+{
+  //init
+   double res=0;
+   double curVal1,curVal2;
+   double curPos;
+   double temp1;
+   int count;
+   int i,j,k;
+   double lowerSum=0;
+   double upperSum=0;
+   double lowerSumOutside=0;
+   double upperSumOutside=0;
+   // structure difference and weight
+   std::vector< std::pair<double,double> > offsetsWeightsLower;
+   std::vector< std::pair<double,double> > offsetsWeightsUpper;
+   //place start of windows before
+   //start of histogram so we can start the loop
+   if (loc1[0]<loc2[0])
+   {
+       curPos=loc1[0]-1.0;
+   }
+   else
+   {
+       curPos=loc2[0]-1.0;
+   }
+   // current value of histogram 1 an 2
+   double curloc1ValStart=0;
+   double curloc1ValEnd=val1[0];
+   double curloc2ValStart=0;
+   double curloc2ValEnd=val2[0];
+   curVal1=0;
+   curVal2=0;
+   // stores the result
+   res=0;
+   //TODO be worried about adding lots of small numbers
+
+   // current location on hist 1 and hist 2
+   i=0;
+   j=0;
+   double value;
+   double offset;
+   std::clock_t start;
+   double duration;
+   start = std::clock();
+   while (1)
+    {
+        if (curloc1ValEnd<curloc2ValEnd)
+        {
+            if (curloc1ValStart<curloc2ValStart)
+            {value=curloc1ValEnd-curloc2ValStart;}
+            else
+            { value=curloc1ValEnd-curloc1ValStart;}
+            offset=loc2[j]-loc1[i];
+            if (value>0.0000000000001) // need to discuss this constant
+            {
+                std::pair<double,double> temp1;
+                temp1.first=offset;
+                temp1.second=value;
+                if (offset<0)
+                {
+                    offsetsWeightsLower.push_back(temp1);
+                    lowerSum+=value;
+                }
+                else
+                {
+                    offsetsWeightsUpper.push_back(temp1);
+                    upperSum+=value;
+                }
+            }
+            i+=1;
+            if (i==loc1.size())
+            {break;}
+            curloc1ValStart=curloc1ValEnd;
+            curloc1ValEnd=val1[i];
+        }
+        else
+        {
+            if (curloc1ValStart<curloc2ValStart)
+            {value=curloc2ValEnd-curloc2ValStart;}
+            else
+            {value=curloc2ValEnd-curloc1ValStart;}
+            offset=loc2[j]-loc1[i];
+            if (value>0.0000000000001)
+            {
+                std::pair<double,double> temp1;
+                temp1.first=offset;
+                temp1.second=value;
+                if (offset<0)
+                {
+                    offsetsWeightsLower.push_back(temp1);
+                    lowerSum+=value;
+                }
+                else
+                {
+                    offsetsWeightsUpper.push_back(temp1);
+                    upperSum+=value;
+                }
+            }
+            j+=1;
+            if (j==loc2.size())
+            {break;}
+            curloc2ValStart=curloc2ValEnd;
+            curloc2ValEnd=val2[j];
+        }
+    }
+    if (i<loc1.size())
+    {
+        for (k=i+1;k<loc1.size();k++)
+        {
+            if (curloc1ValStart<curloc2ValStart)
+            {value=curloc1ValEnd-curloc2ValStart;}
+            else
+            { value=curloc1ValEnd-curloc1ValStart;}
+            offset=loc2[loc2.size()-1]-loc1[k];
+            if (value>0.0000000000001)
+            {
+                std::pair<double,double> temp1;
+                temp1.first=offset;
+                temp1.second=value;
+                if (offset<0)
+                {
+                    offsetsWeightsLower.push_back(temp1);
+                    lowerSum+=value;
+                }
+                else
+                {
+                    offsetsWeightsUpper.push_back(temp1);
+                    upperSum+=value;
+                }
+            }
+            curloc1ValStart=curloc1ValEnd;
+            curloc1ValEnd=val1[k];
+        }
+    }
+    else
+    {
+        for (k=j+1;k<loc2.size();k++)
+        {
+            if (curloc1ValStart<curloc2ValStart)
+            {value=curloc2ValEnd-curloc2ValStart;}
+            else
+            {value=curloc2ValEnd-curloc1ValStart;}
+            offset=loc2[k]-loc1[loc1.size()-1];
+            if (value>0.0000000000001)
+            {
+                std::pair<double,double> temp1;
+                temp1.first=offset;
+                temp1.second=value;
+                if (offset<0)
+                {
+                    offsetsWeightsLower.push_back(temp1);
+                    lowerSum+=value;
+                }
+                else
+                {
+                    offsetsWeightsUpper.push_back(temp1);
+                    upperSum+=value;
+                }
+            }
+            curloc2ValStart=curloc2ValEnd;
+            curloc2ValEnd=val2[k];
+        }
+    }
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+
+  //  std::cout<<"largeLoop: "<< duration <<'\n';
+    NumericVector result(2);
+
+    start = std::clock();
+
+    double curBest;
+    NumericVector loc0;
+    if (lowerSum>upperSum)
+    {
+        std::sort(offsetsWeightsLower.begin(),offsetsWeightsLower.end(),pairComp);
+        duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+        double currentWeight=lowerSum;
+        start = std::clock();
+        for (i=0;i<offsetsWeights.size();i++)
+        for (i=offsetsWeights.size()-1,i>=0;i--)
+        {
+            value=offsetsWeightsLower[i].second;
+            currentWeight-=value;
+            if (currentWeight<0.5)
+            {
+                loc0=loc1+offsetsWeightsLower[i].first;
+                NumericVector result(2);
+                result(0)=NetEmdConstantMedianVersion(loc0,val1,loc2,val2);
+                result(1)=offsetsWeights[i].first;
+                duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+                return result;
+            }
+        }
+    }
+    else
+    {
+        std::sort(offsetsWeightsUpper.begin(),offsetsWeightsUpper.end(),pairComp);
+        duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+        double currentWeight=UpperSum;
+        start = std::clock();
+        for (i=0;i<offsetsWeights.size();i++)
+        {
+            value=offsetsWeightsUpper[i].second;
+            currentWeight-=value;
+            if (currentWeight<0.5)
+            {
+                loc0=loc1+offsetsWeightsUpper[i].first;
+                NumericVector result(2);
+                result(0)=NetEmdConstantMedianVersion(loc0,val1,loc2,val2);
+                result(1)=offsetsWeights[i].first;
+                duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+                return result;
+            }
+        }
+    }
+}
+
