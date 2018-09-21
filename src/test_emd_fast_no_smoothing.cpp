0442a49175cc37497be67c3f80e8131782c644bd
Martin O'Reilly <developer@mtspace.net>
2018-09-21 01:25:42 +0100

    Make Kahan test values fall within 32-bit float range
    Trying to fix failing Kahan test on Appveyor i386
    double should still be 64-bit on 32-bit machines so
    don't really understand why this would fix things.

--- src/test_emd_fast_no_smoothing.cpp
+++ src/test_emd_fast_no_smoothing.cpp
@@ -53,16 +53,18 @@ context("emd_fast_no_smoothing") {
     // (B) If the start number is too small in comparison to the elements in the
     //     small number vector, we don't run into the floating point 
     //     representation issue when we add each element to the running total.
-    // NOTE: Values selected here are those used in the section on the "Kahan 
-    //       summation formula" at https://benjaminjurke.com/content/articles/2015/loss-of-significance-in-floating-point-computations/
+    // NOTE: Appveyor tests with both 32 and 64 bit build sof R (i386 and ix64 
+    //       respectively). Therefore values have been chosen that all fall
+    //       within a 32-bit floating point range (including the expected total)
+    //       while still causing the naiive summation to fail with 64-bit R
     // =========================
     // 1. Define all test data components as powers of a common base to make
     // it easy to accurately calculate the expected sum without adding any small
     // numbers together
     double shared_base = 2.0;
-    double power_start_num = 0.0;
-    double power_num_elements = 8.0;
-    double power_elment_value = -53.0;
+    double power_start_num = 28.0;
+    double power_num_elements = 12.0;
+    double power_elment_value = -28.0;
     // 2. Set up test data
     double start_num = pow(shared_base, power_start_num);
     int_fast64_t num_elements = pow(shared_base, power_num_elements);
