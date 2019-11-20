761898d6be4a7ec2c60ea007fc783e833e226e9d
ande <ande.elliott@gmail.com>
2019-11-20 14:34:44 +0000

    Inital working version of fastEmdSmooth
    Very inefficient (double for loop), but it works and can easily be refined

---
+++ src/fastSmoothV2.cpp
@@ -0,0 +1,268 @@
+// Enable C++11
+// [[Rcpp::plugins(cpp11)]]
+#include <Rcpp.h>
+#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+using namespace Rcpp;
+
+
+double get_segment(double start,double end,double val1_start,double val1_end,double val2_start,double val2_end)
+{
+  double length;
+  length = end - start;
+  double topTriangle;
+  double topRectangle;
+  double bottomTriangle;
+  double midPoint;
+  double midValue;
+  double res=0;
+  if (val1_start > val2_start) 
+  {
+    if (val1_end >= val2_end) 
+    {
+     // They are in the same order no bowtie
+     // top triangle  
+     std::cout << "\n       Path1";
+      topTriangle = 0.5*length*(val1_end-val1_start);
+      topRectangle = length*(val1_start-val2_start);
+      bottomTriangle = 0.5*length*(val2_end-val2_start); 
+      return topTriangle+topRectangle-bottomTriangle;
+    }
+    else
+    {
+        std::cout << "\n       Path2";
+        //bowtie
+        // lets make this really simple as the compiler 
+        // will combine the expressions as needed
+        midPoint = (val1_start-val2_start)/((val2_end-val2_start) - (val1_end-val1_start));
+        midValue = val1_start + midPoint*(val1_end-val1_start);
+        midPoint = midPoint*length;
+        std::cout << "\n       midPoint: " << midPoint << " midValue: " << midValue << "\n";
+        
+        topTriangle = 0.5*midPoint*(midValue-val1_start);
+        topRectangle = midPoint*(val1_start-val2_start);
+        bottomTriangle = 0.5*midPoint*(midValue-val2_start); 
+        
+        res = topTriangle+topRectangle-bottomTriangle;
+        
+        topTriangle = 0.5*(length-midPoint)*(val2_end-midValue);
+        topRectangle = 0; // midPoint*(val1_start-val2_start);
+        bottomTriangle = 0.5*(length - midPoint)*(val1_end - midValue); 
+        res += topTriangle+topRectangle-bottomTriangle;
+        return res;
+    }
+  }
+  else
+    {
+      if (val1_end > val2_end) 
+      {
+     std::cout << "\n       Path3";
+        //bowtie
+        midPoint = (val2_start-val1_start)/((val1_end-val1_start) - (val2_end-val2_start));
+        midValue = val2_start + midPoint*(val2_end-val2_start);
+        midPoint = midPoint*length;
+        std::cout << "\n       midPoint: " << midPoint << " midValue: " << midValue << "\n";
+        
+        topTriangle = 0.5*midPoint*(midValue-val2_start);
+        topRectangle = midPoint*(val2_start-val1_start);
+        bottomTriangle = 0.5*midPoint*(midValue-val1_start); 
+        
+        res = topTriangle+topRectangle-bottomTriangle;
+        
+        topTriangle = 0.5*(length-midPoint)*(val1_end-midValue);
+        topRectangle = 0; // midPoint*(val1_start-val2_start);
+        bottomTriangle = 0.5*(length - midPoint)*(val2_end - midValue); 
+        res += topTriangle+topRectangle-bottomTriangle;
+        return res;
+        
+      }
+      else // same order
+      {
+        std::cout << "\n       Path4";
+        topTriangle = 0.5*length*(val2_end-val2_start);
+        topRectangle = length*(val2_start-val1_start);
+        bottomTriangle = 0.5*length*(val1_end-val1_start); 
+        return topTriangle+topRectangle-bottomTriangle;
+      }
+    }
+}
+
+
+//' @title
+//' Compute EMD
+////'
+////' @param loc1 numeric vector.
+////' @param val1 numeric vector.
+////' @param loc2 numeric vector.
+////' @param val2 numeric vector.
+//'
+//' @export
+// [[Rcpp::export]]
+double NetEmdSmoothV2(NumericVector loc1,NumericVector val1,double binWidth1,NumericVector loc2,NumericVector val2,double binWidth2)
+{
+  double loc1_start, loc1_end, loc2_start, loc2_end;
+  double val1_start, val1_end, val2_start, val2_end;
+  int index1,index2;
+  
+  // Index in both lists
+  index1 = 0;
+  index2 = 0;
+  
+  
+  // Loc1 data
+  loc1_start = loc1[0];
+  loc1_end = loc1[0];
+  val1_start = 0;
+  val1_end   = 0; 
+  
+  // Loc2 data
+  loc2_start = loc2[0];
+  loc2_end   = loc2[0];
+  val2_start = 0;
+  val2_end   = 0; 
+  
+  double endX;
+  endX = std::min(loc2_end,loc1_end); 
+  // while statement
+  double res=0;
+  
+  // Hist 1
+  double curSeg1Loc1; 
+  double curSeg1Loc2; 
+  double curSeg1Val1; 
+  double curSeg1Val2; 
+  
+  // Hist 1
+  double curSeg2Loc1; 
+  double curSeg2Loc2; 
+  double curSeg2Val1; 
+  double curSeg2Val2; 
+  
+  // need to iterate through regions of constant gradient
+  double h1,h2;
+  h1=0;
+  h2=0;
+  int i123,j123;
+  double tempStart;
+  double tempEnd;
+  double valStart1;
+  double valEnd1;
+  double valStart2;
+  double valEnd2;
+  double tempDouble;
+  for (index1=-1;index1<loc1.size();index1++) 
+  {
+    for (index2=-1;index2<loc2.size();index2++) 
+    {
+              for (i123=0;i123<2;i123++) 
+              {
+              if (index1==-1)
+              {
+                  if (i123==1)
+                  {continue;}
+                          curSeg1Loc1=std::min(loc1[0],loc2[0]); 
+                          curSeg1Loc2=loc1[0]; 
+                          curSeg1Val1=0;
+                          curSeg1Val2=0;
+              }
+              else
+              {
+                  if (i123==0)
+                  {
+                    curSeg1Loc1=loc1[index1]; 
+                    curSeg1Loc2=loc1[index1]+binWidth1; 
+                    if (index1==0)
+                      {curSeg1Val1=0;}
+                    else
+                      {curSeg1Val1=val1[index1-1];}
+                    curSeg1Val2=val1[index1]; 
+                  }
+                  else
+                  {
+                    curSeg1Loc1=loc1[index1]+binWidth1; 
+                    if (index1==loc1.size()-1)
+                      {curSeg1Loc2=std::max(loc1[loc1.size()-1] +binWidth1,loc2[loc2.size()-1]+binWidth2 ); }
+                    else
+                      {curSeg1Loc2=loc1[index1+1];}
+                    curSeg1Val1=val1[index1]; 
+                    curSeg1Val2=val1[index1]; 
+                  }
+              }
+        
+        
+        for (j123=0;j123<2;j123++) 
+        {
+            if (index2==-1)
+            {
+              if (j123==1)
+              {continue;}
+                        curSeg2Loc1=std::min(loc1[0],loc2[0]); 
+                        curSeg2Loc2=loc2[0]; 
+                        curSeg2Val1=0;
+                        curSeg2Val2=0;
+            }
+            else
+            {
+          
+              if (j123==0)
+              {
+                curSeg2Loc1=loc2[index2]; 
+                curSeg2Loc2=loc2[index2]+binWidth2; 
+                if (index2==0)
+                  {curSeg2Val1=0;}
+                else
+                  {curSeg2Val1=val2[index2-1];}
+                curSeg2Val2=val2[index2]; 
+              }
+              else
+              {
+                curSeg2Loc1=loc2[index2]+binWidth2; 
+                if (index2==loc2.size()-1)
+                  {curSeg2Loc2=std::max(loc1[loc1.size()-1] +binWidth1,loc2[loc2.size()-1]+binWidth2 ); }
+                else
+                  {curSeg2Loc2=loc2[index2+1];}
+                curSeg2Val1=val2[index2]; 
+                curSeg2Val2=val2[index2]; 
+              }
+          }
+          
+          tempStart = std::max(curSeg1Loc1,curSeg2Loc1); 
+          tempEnd   = std::min(curSeg1Loc2,curSeg2Loc2); 
+          if (tempStart<tempEnd)
+          {
+            //We have a valid range
+          // double get_segment(double start,double end,double val1_start,double val1_end,double val2_start,double val2_end)
+          
+            valStart1 = curSeg1Val1 + (curSeg1Val2-curSeg1Val1)*(tempStart - curSeg1Loc1)/(curSeg1Loc2 - curSeg1Loc1);
+            valEnd1   = curSeg1Val1 + (curSeg1Val2-curSeg1Val1)*(tempEnd   - curSeg1Loc1)/(curSeg1Loc2 - curSeg1Loc1);
+            valStart2 = curSeg2Val1 + (curSeg2Val2-curSeg2Val1)*(tempStart - curSeg2Loc1)/(curSeg2Loc2 - curSeg2Loc1);
+            valEnd2   = curSeg2Val1 + (curSeg2Val2-curSeg2Val1)*(tempEnd   - curSeg2Loc1)/(curSeg2Loc2 - curSeg2Loc1);
+            std::cout << "\nSeg1 : " << curSeg1Loc1 << ", " << curSeg1Loc2 << "";
+            std::cout << "\n    Seg2 : " << curSeg2Loc1 << ", " << curSeg2Loc2 << "";
+            tempDouble = get_segment(tempStart,tempEnd,valStart1,valEnd1,valStart2,valEnd2);
+            std::cout << "\n        curSeg1Val1: " << curSeg1Val1 <<" ";
+            std::cout << "\n        curSeg1Val2: " << curSeg1Val2 <<" ";
+            std::cout << "\n        curSeg2Val1: " << curSeg2Val1 <<" ";
+            std::cout << "\n        curSeg2Val2: " << curSeg2Val2 <<" ";
+            std::cout << "\n        valEnd1:     " << valEnd1 <<" ";
+            std::cout << "\n        tempStart:   " << tempStart <<" ";
+            std::cout << "\n        tempEnd:     " << tempEnd <<" ";
+            std::cout << "\n        valStart1:   " << valStart1 <<" ";
+            std::cout << "\n        valEnd1:     " << valEnd1 <<" ";
+            std::cout << "\n        valStart2:   " << valStart2 <<" ";
+            std::cout << "\n        valEnd2:     " << valEnd2 <<" ";
+            std::cout << "\n        res: " << tempDouble <<" \n";
+            res += tempDouble;  
+          }
+//          else
+//          {
+//            std::cout << "\n        res: " << 0 <<" \n";
+//          }
+        }
+      }
+    }
+  }
+  return res;
+}
---
+++ tests/testthat/test_fastEMD.R
@@ -0,0 +1,154 @@
+context("Name of test context")
+
+library(Rcpp)
+makeFunction <- function(x1,v1,w1,x2,v2,w2)
+{
+  f1 <- function(x)
+  {
+    res1 = rep(0,length(x))
+    m1 = x1[1]<x
+    m2 = x<=(x1[1]+w1)
+    m3 = m1*m2
+    res1 = res1 + m3*(x-x1[1])*(v1[1]-0)/w1
+    
+    m1 = (x1[1]+w1)<x
+    m2 = x<=(x1[2])
+    m3 = m1*m2
+    res1 = res1 + m3*v1[1] 
+    
+    for (i in 2:length(x1))
+    {
+      m1 = x1[i]<x
+      m2 = x<=(x1[i]+w1)
+      m3 = m1*m2
+      res1 = res1 + m3*((x-x1[i])*(v1[i]-v1[i-1])/w1+v1[i-1])
+      if (i<length(x1)) 
+      {
+        m1 = (x1[i]+w1)<x
+        m2 = x<=(x1[i+1])
+        m3 = m1*m2
+        res1 = res1 + m3*v1[i]
+      }
+
+    }
+    m1 = x>(x1[length(v1)]+w1)
+    res1 = res1 + m1*v1[length(v1)]
+
+    
+    
+    res2= rep(0,length(x))
+    m1 = x2[1]<x
+    m2 = x<=(x2[1]+w2)
+    m3 = m1*m2
+    res2 = m3*(x-x2[1])*(v2[1]-0)/w2
+    
+    
+    m1 = (x2[1]+w2)<x
+    m2 = x<=(x2[2])
+    m3 = m1*m2
+    res2 = res2 + m3*v2[1] 
+    
+    for (i in 2:length(x2))
+    {
+      m1 = x2[i]<x
+      m2 = x<=(x2[i]+w2)
+      m3 = m1*m2
+      res2 = res2 + m3*((x-x2[i])*(v2[i]-v2[i-1])/w2+v2[i-1])
+      if (i<length(x2)) 
+      {
+        m1 = (x2[i]+w2)<x
+        m2 = x<=(x2[i+1])
+        m3 = m1*m2
+        res2 = res2 + m3*v2[i]
+      }
+    }
+    m1 = x>(w2+x2[length(v2)])
+    res2 = res2 + m1*v2[length(v2)]
+    
+    abs(res1-res2)
+  }
+  f1
+}
+
+test_that("3 element test", {
+  
+            sourceCpp("~/Documents/network-comparison/src/fastSmoothV2.cpp")
+            for (w1 in (1:10)/10.0)
+            {
+              for (w2 in (1:10)/10.0)
+              {
+                x1 <- c(1,2,3)
+                v1 <- c(0.25,0.70,1.00)
+                x2 <- c(1,2,3)
+                v2 <- c(0.25,0.70,1.00)
+                print(x1)
+                f1 <- makeFunction(x1,v1,w1,x2,v2,w2)
+                res2 <- integrate(f1,0,max(x2[3],x1[3])+max(w1,w2),abs.tol=0.000000001)[[1]]
+                
+                res1 <- NetEmdSmoothV2(x1,v1,w1,x2,v2,w2)
+                
+                expect_lt(abs(res1-res2),10**(-3))
+              }
+            }
+            })
+
+test_that("2 element test w1=0.1, w2=0.2", {
+  
+  sourceCpp("~/Documents/network-comparison/src/fastSmoothV2.cpp")
+  w1 = 0.1
+  w2 = 0.2
+  x1 <- c(1,2)
+  v1 <- c(0.25,0.75)
+  x2 <- c(1,2)
+  v2 <- c(0.5,1.00)
+  print(x1)
+  f1 <- makeFunction(x1,v1,w1,x2,v2,w2)
+  res2 <- integrate(f1,0,max(x2[3],x1[3])+max(w1,w2),abs.tol=0.000000001)[[1]]
+  res1 <- NetEmdSmoothV2(x1,v1,w1,x2,v2,w2)
+  
+  expect_lt(abs(res1-res2),10**(-4))
+})
+
+
+
+test_that("3 element test Mixture", {
+  
+            sourceCpp("~/Documents/network-comparison/src/fastSmoothV2.cpp")
+            for (w1 in (1:10)/10.0)
+            {
+              for (w2 in (1:10)/10.0)
+              {
+                x1 <- c(1,2,3)
+                v1 <- c(0.65,0.70,1.00)
+                x2 <- c(1,2,3)
+                v2 <- c(0.25,0.70,1.00)
+                print(x1)
+                f1 <- makeFunction(x1,v1,w1,x2,v2,w2)
+                res2 <- integrate(f1,0,max(x2[3],x1[3])+max(w1,w2),abs.tol=0.000000001)[[1]]
+                
+                res1 <- NetEmdSmoothV2(x1,v1,w1,x2,v2,w2)
+                
+                expect_lt(abs(res1-res2),10**(-3))
+              }
+            }
+            })
+
+
+
+
+test_that("3 element test w1=0.1, w2=0.2", {
+  
+            sourceCpp("~/Documents/network-comparison/src/fastSmoothV2.cpp")
+  w1 = 0.1
+  w2 = 0.2
+              x1 <- c(1,2,3)
+              v1 <- c(0.25,0.70,1.00)
+              x2 <- c(1,2,3)
+              v2 <- c(0.25,0.70,1.00)
+              print(x1)
+              f1 <- makeFunction(x1,v1,w1,x2,v2,w2)
+              res2 <- integrate(f1,0,max(x2[3],x1[3])+max(w1,w2),abs.tol=0.000000001)[[1]]
+              res1 <- NetEmdSmoothV2(x1,v1,w1,x2,v2,w2)
+              
+              expect_lt(abs(res1-res2),10**(-4))
+            })
