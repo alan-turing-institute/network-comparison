857c91ce2b459467b850716fd00b09c7a66d1b6b
Oliver Strickson <ostrickson@turing.ac.uk>
2020-04-23 11:04:43 +0100

    Small refactor of OverlappingSegments, and use for NetEmdSmoothV2

--- src/fastSmoothV2.cpp
+++ src/fastSmoothV2.cpp
@@ -10,21 +10,21 @@
 using namespace Rcpp;
 
 double bowtie_area(double length, double val1_start, double val1_end,
-                          double val2_start, double val2_end)
+                   double val2_start, double val2_end)
 {
   double midPoint = (val1_start - val2_start) /
     ((val2_end - val2_start) - (val1_end - val1_start));
 
   const double midValue = val1_start + midPoint * (val1_end - val1_start);
-    
+
   midPoint = midPoint * length;
-    
+
   double topTriangle = 0.5 * midPoint * (midValue - val1_start);
   double topRectangle = midPoint * (val1_start - val2_start);
   double bottomTriangle = 0.5 * midPoint * (midValue - val2_start);
-    
+
   double res = topTriangle + topRectangle - bottomTriangle;
-    
+
   topTriangle = 0.5 * (length - midPoint) * (val2_end - midValue);
   topRectangle = 0; // midPoint*(val1_start-val2_start);
   bottomTriangle = 0.5 * (length - midPoint) * (val1_end - midValue);
@@ -36,7 +36,7 @@ double bowtie_area(double length, double val1_start, double val1_end,
 // Compute the unsigned area between two line segments
 // assumes that val1_end > val1_start and val2_end > val2_start
 double get_segment(double start, double end, double val1_start,
-                          double val1_end, double val2_start, double val2_end)
+                   double val1_end, double val2_start, double val2_end)
 {
   const double length = end - start;
 
@@ -49,7 +49,7 @@ double get_segment(double start, double end, double val1_start,
 
   bool both_differences_positive = val1_start > val2_start && val1_end >= val2_end;
   bool both_differences_negative = val1_start <= val2_start && val1_end <= val2_end;
-  
+
   if (both_differences_positive || both_differences_negative)
   {
     // They are in the same order: no bowtie
@@ -73,9 +73,9 @@ double get_segment(double start, double end, double val1_start,
 
 // cut down and compute segment
 double get_segment_constrained(double seg1L1, double seg1L2,
-                                      double seg2L1, double seg2L2,
-                                      double seg1V1, double seg1V2,
-                                      double seg2V1, double seg2V2)
+                               double seg2L1, double seg2L2,
+                               double seg1V1, double seg1V2,
+                               double seg2V1, double seg2V2)
 {
   //We have a valid range
   double valStart1, valEnd1, valStart2, valEnd2;
@@ -102,85 +102,29 @@ double get_double_segment_constrained(
   double seg2Loc1, double seg2Loc2, double seg2Loc3,
   double seg2Val1, double seg2Val2)
 {
-    double res = 0;
+  double res = 0;
 
-    // compare the linear section with the linear section
-    res += get_segment_constrained(seg1Loc1, seg1Loc2, seg2Loc1, seg2Loc2,
-                                   seg1Val1, seg1Val2, seg2Val1, seg2Val2);
+  // compare the linear section with the linear section
+  res += get_segment_constrained(seg1Loc1, seg1Loc2, seg2Loc1, seg2Loc2,
+                                 seg1Val1, seg1Val2, seg2Val1, seg2Val2);
 
-    // compare the linear section with the flat section
-    // This could be easily special cased (saving ~1 if statements ).
-    res += get_segment_constrained(seg1Loc1, seg1Loc2, seg2Loc2, seg2Loc3,
-                                   seg1Val1, seg1Val2, seg2Val2, seg2Val2);
+  // compare the linear section with the flat section
+  // This could be easily special cased (saving ~1 if statements ).
+  res += get_segment_constrained(seg1Loc1, seg1Loc2, seg2Loc2, seg2Loc3,
+                                 seg1Val1, seg1Val2, seg2Val2, seg2Val2);
 
-    // compare the flat section with the linear section
-    // This could be easily special cased (saving ~1 if statements ).
-    res += get_segment_constrained(seg1Loc2, seg1Loc3, seg2Loc1, seg2Loc2,
-                                   seg1Val2, seg1Val2, seg2Val1, seg2Val2);
+  // compare the flat section with the linear section
+  // This could be easily special cased (saving ~1 if statements ).
+  res += get_segment_constrained(seg1Loc2, seg1Loc3, seg2Loc1, seg2Loc2,
+                                 seg1Val2, seg1Val2, seg2Val1, seg2Val2);
 
-    // compare the flat section with the flat section
-    // This could be easily special cased (saving ~2 if statements ).
-    res += get_segment_constrained(seg1Loc2, seg1Loc3, seg2Loc2, seg2Loc3,
-                                   seg1Val2, seg1Val2, seg2Val2, seg2Val2);
-
-    return res;
-}
-
-
-// Dealing with segments which are to the left of the region covered by both
-// segs
-double leftmost_segments(const NumericVector& loc1,
-                                const NumericVector& loc2,
-                                const NumericVector& val1,
-                                const NumericVector& val2,
-                                double binWidth1,
-                                double maxLoc)
-{
-  double res = 0.0;
-
-  assert(loc1[0] < loc2[0]);
-
-  // Fix the position of Seg2 and then interate over Seg1 until we have all
-  // of the segments of Seg1 before Seg2 starts.
-
-  // are these all used?
-  
-  double curSeg2Loc1 = loc1[0];
-  double curSeg2Loc2 = loc1[0];
-  double curSeg2Loc3 = loc2[0];
-  double curSeg2Val1 = 0;
-  double curSeg2Val2 = 0;
-
-  // Set this value so we can update in the lopp
-  double curSeg1Val2 = 0;
-  for (int index = 0; index < loc1.size(); index++) {
-    double curSeg1Loc1 = loc1[index];
-    double curSeg1Loc2 = loc1[index] + binWidth1;
-    double curSeg1Loc3;
-    if (index == loc1.size() - 1) {
-      curSeg1Loc3 = maxLoc;
-    }
-    else {
-      curSeg1Loc3 = loc1[index + 1];
-    }
-    double curSeg1Val1 = curSeg1Val2;
-    curSeg1Val2 = val1[index];
-    res += get_double_segment_constrained(
-      curSeg1Loc1, curSeg1Loc2, curSeg1Loc3, curSeg1Val1, curSeg1Val2,
-      curSeg2Loc1, curSeg2Loc2, curSeg2Loc3, curSeg2Val1, curSeg2Val2);
-
-    std::cerr << "%% " << index << " : " << curSeg1Loc1 << " " << curSeg1Loc2 << " " << curSeg1Loc3 << " "
-              << curSeg1Val1 << " " << curSeg1Val2 << " " << curSeg2Loc1 << " " << curSeg2Loc2
-              << " " << curSeg2Loc3 << " " << curSeg2Val1 << " " << curSeg2Val2 << std::endl;
-    
-    if (curSeg2Loc1 > curSeg1Loc3) {
-      break;
-    }
-  }
+  // compare the flat section with the flat section
+  // This could be easily special cased (saving ~2 if statements ).
+  res += get_segment_constrained(seg1Loc2, seg1Loc3, seg2Loc2, seg2Loc3,
+                                 seg1Val2, seg1Val2, seg2Val2, seg2Val2);
 
   return res;
 }
-
 
 //' @title
 //' Compute EMD
@@ -195,160 +139,55 @@ double leftmost_segments(const NumericVector& loc1,
 double NetEmdSmoothV2(NumericVector loc1, NumericVector val1, double binWidth1,
                       NumericVector loc2, NumericVector val2, double binWidth2)
 {
-  // double res = 0;
+  OverlappingSegments segs(loc1, loc2, binWidth1, binWidth2);
 
-  // // Hist 1
-  // double curSeg1Loc1; // Start of the gradient section in Seg1
-  // double curSeg1Loc2; // End of the gradient section in Seg1
-  // double curSeg1Loc3; // End of the flat section in Seg1
-  // double curSeg1Val1; // Start value in Seg1
-  // double curSeg1Val2; // End value in Seg1
+  double res = 0.0;
 
-  // // Hist 2
-  // double curSeg2Loc1; // Start of the gradient section in Seg2
-  // double curSeg2Loc2; // End of the gradient section in Seg2
-  // double curSeg2Loc3; // End of the flat section in Seg2
-  // double curSeg2Val1; // Start value in Seg2
-  // double curSeg2Val2; // End value in Seg2
-
-
-  // // Starting index for the second histogram
-  // double secondStart = 0;
-
-  // // Smallest and largest location values
-  // double maxLoc = std::max(loc1[loc1.size()-1] + binWidth1,
-  //                          loc2[loc2.size()-1] + binWidth2);
-
-
-  // // warning area before loc2[0] is not well tested
-  // // As values outside of the range appear to be zero
-
-  // std::cerr << "===============\n";
-  
-  // if (loc2[0] < loc1[0]) {
-  //   res += leftmost_segments(loc2, loc1, val2, val1, binWidth2, maxLoc);
-  // }
-  // else
-  // {
-  //   res += leftmost_segments(loc1, loc2, val1, val2, binWidth1, maxLoc);
-  // }
-
-  // // Add both the  overlapping sections and the non overlapping section on the right
-  // // Note we reiterate over the first few sections loc1
-  // // Could store where we are upto from above to save time
-  // // Reset Val counter
-  // curSeg1Val2 = 0;
-  // for (int index1 = 0; index1 < loc1.size(); index1++) {
-  //   //  Get the three relevant locations
-  //   // Start; end of linear section; end of flat section
-  //   curSeg1Loc1 = loc1[index1];
-  //   curSeg1Loc2 = loc1[index1] + binWidth1;
-  //   // could pull this check outside of the loop with final case not sure if worth it
-  //   if (index1 == loc1.size() - 1) {
-  //     curSeg1Loc3 = maxLoc;
-  //   }
-  //   else {
-  //     curSeg1Loc3 = loc1[index1 + 1];
-  //   }
-  //   // Update value to the start and end of the current section
-  //   curSeg1Val1 = curSeg1Val2;
-  //   curSeg1Val2 = val1[index1];
-  //   // Setting up the previous value for the next loop
-  //   // Could replace this loop with a while
-  //   // but if so would need to be careful about overlaps
-  //   if (secondStart == 0) {
-  //     curSeg2Val2 = 0;
-  //   }
-  //   else {
-  //     curSeg2Val2 = val2[secondStart - 1];
-  //   }
-  //   for (int index2 = secondStart; index2 < loc2.size(); index2++) {
-  //     // Construct 3 sections for second seg.
-  //     curSeg2Loc1 = loc2[index2];
-  //     curSeg2Loc2 = loc2[index2] + binWidth2;
-  //     if (index2 == loc2.size() - 1) {
-  //       curSeg2Loc3 = maxLoc;
-  //     }
-  //     else {
-  //       curSeg2Loc3 = loc2[index2 + 1];
-  //     }
-  //     //update values
-  //     curSeg2Val1 = curSeg2Val2;
-  //     curSeg2Val2 = val2[index2];
-  //     // If this section is behind Seg1
-  //     // Do not consider again
-  //     if (curSeg2Loc3 < curSeg1Loc1) {
-  //       secondStart = index2 + 1;
-  //       continue;
-  //     }
-  //     // If current Seg2 is beyond Seg1 break out of loop
-  //     res += get_double_segment_constrained(
-  //       curSeg1Loc1, curSeg1Loc2, curSeg1Loc3, curSeg1Val1, curSeg1Val2,
-  //       curSeg2Loc1, curSeg2Loc2, curSeg2Loc3, curSeg2Val1, curSeg2Val2);
-
-  //     std::cerr << "%% " << index1 << " " << index2 << " : " << curSeg1Loc1 << " " << curSeg1Loc2 << " " << curSeg1Loc3 << " "
-  //               << curSeg1Val1 << " " << curSeg1Val2 << " " << curSeg2Loc1 << " " << curSeg2Loc2
-  //               << " " << curSeg2Loc3 << " " << curSeg2Val1 << " " << curSeg2Val2 << std::endl;
-      
-  //     if (curSeg2Loc3 > curSeg1Loc3) {
-  //       break;
-  //     }
-  //   }
-  // }
-
-  // std::cerr << "---------------\n";
- 
+  for (OverlappingSegments::iterator it = segs.begin(), endIt = segs.end();
+       it != endIt; ++it)
   {
-    OverlappingSegments segs(loc1, loc2, binWidth1, binWidth2);
+    // The OverlappingSegments iterator returns pairs of the left
+    // indices of overlapping endpoints: it->first is the index into
+    // loc1, it->second is the index into loc2.  When the smallest
+    // element in one of these vectors is larger than the current
+    // element of the other, an 'index' of -1 is returned.
 
-    double res = 0.0;
-  
-    for (OverlappingSegments::iterator it = segs.begin(), endIt = segs.end();
-         it != endIt; ++it)
-    {
-      // Hist 1
-      // Start of the gradient section in Seg1
-      double curSeg1Loc1 = it.get_loc1(it->first);
+    // Hist 1
+    // Start of the gradient section in Seg1
+    double curSeg1Loc1 = segs.loc1_left(it->first);
 
-      // End of the gradient section in Seg1
-      double curSeg1Loc2 = (it->first < 0) ? curSeg1Loc1 : (curSeg1Loc1 + binWidth1);
+    // End of the gradient section in Seg1
+    double curSeg1Loc2 = segs.loc1_mid(it->first);
 
-      // End of the flat section in Seg1
-      double curSeg1Loc3 = it.get_loc1(it->first + 1);
+    // End of the flat section in Seg1
+    double curSeg1Loc3 = segs.loc1_right(it->first);
 
-      // Start value in Seg1
-      double curSeg1Val1 = (it->first <= 0) ? 0.0 : val1[it->first - 1];
+    // Start and end values in Seg1: val1 gives the values at *right*
+    // endpoints of the segments.  A value of 0.0 is used before the
+    // first segment.
+    double curSeg1Val1 = (it->first > 0) ? val1[it->first - 1] : 0.0;
+    double curSeg1Val2 = (it->first >= 0) ? val1[it->first] : 0.0;
 
-      // End value in Seg1
-      double curSeg1Val2 = (it->first < 0) ? 0.0 : val1[it->first];
+    // Hist 2
+    // Start of the gradient section in Seg1
+    double curSeg2Loc1 = segs.loc2_left(it->second);
 
-      // Hist 2
-      // Start of the gradient section in Seg2
-      double curSeg2Loc1 = it.get_loc2(it->second);
+    // End of the gradient section in Seg1
+    double curSeg2Loc2 = segs.loc2_mid(it->second);
 
-      // End of the gradient section in Seg2
-      double curSeg2Loc2 = (it->second < 0) ? curSeg2Loc1 : (curSeg2Loc1 + binWidth2);
+    // End of the flat section in Seg1
+    double curSeg2Loc3 = segs.loc2_right(it->second);
 
-      // End of the flat section in Seg2
-      double curSeg2Loc3 = it.get_loc2(it->second + 1); 
+    // Start and end values in Seg2: val2 gives the values at *right*
+    // endpoints of the segments.  A value of 0.0 is used before the
+    // first segment.
+    double curSeg2Val1 = (it->second > 0) ? val2[it->second - 1] : 0.0;
+    double curSeg2Val2 = (it->second >= 0) ? val2[it->second] : 0.0;
 
-      // Start value in Seg2
-      double curSeg2Val1 = (it->second <= 0) ? 0.0 : val2[it->second - 1];
-
-      // End value in Seg2
-      double curSeg2Val2 = (it->second < 0) ? 0.0 : val2[it->second];
-
-      // std::cerr << "%% " << it->first << " " << it->second << " : " << curSeg1Loc1 << " " << curSeg1Loc2 << " " << curSeg1Loc3 << " "
-      //           << curSeg1Val1 << " " << curSeg1Val2 << " " << curSeg2Loc1 << " " << curSeg2Loc2
-      //           << " " << curSeg2Loc3 << " " << curSeg2Val1 << " " << curSeg2Val2 << std::endl;
-
-      res += get_double_segment_constrained(
-        curSeg1Loc1, curSeg1Loc2, curSeg1Loc3, curSeg1Val1, curSeg1Val2,
-        curSeg2Loc1, curSeg2Loc2, curSeg2Loc3, curSeg2Val1, curSeg2Val2);    
-    }
-
-    return res;
+    res += get_double_segment_constrained(
+      curSeg1Loc1, curSeg1Loc2, curSeg1Loc3, curSeg1Val1, curSeg1Val2,
+      curSeg2Loc1, curSeg2Loc2, curSeg2Loc3, curSeg2Val1, curSeg2Val2);
   }
 
-  // std::cerr << "===============\n";
+  return res;
 }
--- src/fastSmoothV2.h
+++ src/fastSmoothV2.h
@@ -9,8 +9,6 @@
 #include <cassert>
 #include <utility>
 #include <iterator>
-// #include <vector>
-// #include <iostream>
 #include <stdexcept>
 #include <limits>
 #include <unordered_set>
@@ -21,38 +19,81 @@
 using namespace Rcpp;
 
 class OverlappingSegments {
-  // references the two sequences
+
+  // The two sequences of left-hand segment endpoints
   const NumericVector& loc1;
   const NumericVector& loc2;
   const double binWidth1, binWidth2;
 
+  // shorter names for loc1.size() and loc2.size()
+  const long N1, N2;
+
+  // the minimum and maximum values over both sequences;
+  double minloc, maxloc;
+
 public:
   OverlappingSegments(NumericVector& loc1_, NumericVector& loc2_,
                       double binWidth1_ = 1.0, double binWidth2_ = 1.0)
-    : loc1(loc1_), loc2(loc2_), binWidth1(binWidth1_), binWidth2(binWidth2_)
+    : loc1(loc1_), loc2(loc2_),
+      binWidth1(binWidth1_), binWidth2(binWidth2_),
+      N1(loc1.size()), N2(loc2.size())
   {
-    // check the requirement that loc1 and loc2 are nonempty and
-    // (strictly) sorted
-
-    if (loc1.size() == 0 || loc2.size() == 0)
+    if (N1 == 0 || N2 == 0)
       throw std::invalid_argument("Input vectors must be nonempty");
-    
-    for (int i = 0; i < loc1.size() - 1; i++)
-      if (loc1[i] > loc1[i + 1])
-        throw std::invalid_argument("Input vectors must be sorted in strict ascending order");
 
-    for (int i = 0; i < loc2.size() - 1; i++)
-      if (loc2[i] > loc2[i + 1])
-        throw std::invalid_argument("Input vectors must be sorted in strict ascending order");
+    for (int i = 0; i < N1 - 1; i++)
+      if (loc1[i] > loc1[i + 1] + binWidth1)
+        throw std::invalid_argument(
+          "Elements of loc1 must be sorted in ascending order, "
+          "with elements separated by at least binWidth1");
+
+    for (int i = 0; i < N2 - 1; i++)
+      if (loc2[i] > loc2[i + 1] + binWidth2)
+        throw std::invalid_argument(
+          "Elements of loc2 must be sorted in ascending order, "
+          "with elements separated by at least binWidth2");
+
+    minloc = std::min(loc1[0], loc2[0]);
+    maxloc = std::max(loc1[N1 - 1] + binWidth1, loc2[N2 - 1] + binWidth2);
   }
 
+  // left, mid and right locations of the segments, for loc1 and loc2
+  double loc1_left(long i) const {
+    return (i >= 0) ? loc1[i] : minloc;
+  }
+
+  double loc1_mid(long i) const {
+    return (i >= 0) ? (loc1[i] + binWidth1) : minloc;
+  }
+
+  double loc1_right(long i) const {
+    return (i + 1 < N1) ? loc1[i + 1] : maxloc;
+  }
+
+  double loc2_left(long i) const {
+    return (i >= 0) ? loc2[i] : minloc;
+  }
+
+  double loc2_mid(long i) const {
+    return (i >= 0) ? (loc2[i] + binWidth2) : minloc;
+  }
+
+  double loc2_right(long i) const {
+    return (i + 1 < N2) ? loc2[i + 1] : maxloc;
+  }
+
+  // Does interval i (from the first collection of segments) overlap
+  // interval j (from the second)?
+  bool intervals_overlap(long i, long j) const {
+    return (loc1_left(i) < loc2_right(j) && loc2_left(j) < loc1_right(i));
+  }
+
+  iterator begin() { return iterator(*this); };
+  iterator end() { return iterator(*this).advance_to_end(); };
+
+  // OverlappingSegments iterator
   class iterator {
-    const OverlappingSegments * const segs;
-
-    long Nx, Ny;
-
-    // the minimum and maximum contained values;
-    double minloc, maxloc;
+    const OverlappingSegments& segs;
 
     // the current iteration state
     std::pair<long, long> idx;
@@ -78,99 +119,59 @@ public:
     // other one.  If both sequences start with the same value, the
     // iteration starts at (0,0).
     //
-    explicit iterator(OverlappingSegments *segs_)
-      : segs(segs_), Nx(segs_->loc1.size()), Ny(segs_->loc2.size())
-    {
-      // The initial state of the iterator
-      
-      if (segs->loc1[0] < segs->loc2[0]) {
-        minloc = segs->loc1[0];
+    explicit iterator(const OverlappingSegments& segs_) : segs(segs_) {
+      if (segs.loc1[0] < segs.loc2[0]) {
         idx.first = 0;
         idx.second = -1;
       }
-      else if (segs->loc1[0] == segs->loc2[0]) {
-        minloc = segs->loc1[0]; // == segs->loc2[0]
+      else if (segs.loc1[0] == segs.loc2[0]) {
         idx.first = 0;
         idx.second = 0;
       }
       else {
-        minloc = segs->loc2[0];
         idx.first = -1;
         idx.second = 0;
       }
-
-      // maxloc is used to signal the end of the current segment:
-      // since we consider only left-hand endpoints, the only
-      // requirement is that it compares greater than any real loc in
-      // either sequence - in reality it will be the largest loc plus
-      // the corresponding bin width
-      // maxloc = std::numeric_limits<double>::infinity();
-      maxloc = std::max(segs->loc1[segs->loc1.size() - 1] + segs->binWidth1,
-                        segs->loc2[segs->loc2.size() - 1] + segs->binWidth2);
     }
 
-    double get_loc1(long i) const {
-      if (i < 0) return minloc;
-      else if (i >= Nx) return maxloc;
-      else return segs->loc1[i];
-    }
+    // Is the current iterator at one-past-the-end?  Equivalent to an
+    // equality comparison with segs.end().
+    bool at_end() const { return idx.first == segs.N1 && idx.second == segs.N2 - 1; }
 
-    double get_loc2(long i) const {
-      if (i < 0) return minloc;
-      else if (i >= Ny) return maxloc;
-      else return segs->loc2[i];
-    }
-
-    // Does interval i (from the first collection of segments) overlap
-    // interval j (from the second)?
-    bool intervals_overlap(long i, long j) const {
-      return (get_loc1(i) < get_loc2(j + 1) && get_loc2(j) < get_loc1(i + 1));
-    }
-
-    bool at_end() const { return idx.first == Nx && idx.second == Ny - 1; }
-
+    // Update the current iterator to point to one-past-the-end
     iterator& advance_to_end() {
-      idx.first = Nx;
-      idx.second = Ny - 1;
+      idx.first = segs.N1;
+      idx.second = segs.N2 - 1;
       return *this;
     }
-    
-    value_type operator*() const { return idx; }
-
-    const value_type *operator->() const { return &idx; }
 
     iterator& operator++() {
-
 #if !NDEBUG
       // Verify precondition
-      if (!intervals_overlap(idx.first, idx.second)) {
+      if (!segs.intervals_overlap(idx.first, idx.second)) {
         throw std::logic_error("Iterator precondition not satisfied: "
                                "current intervals do not overlap");
       }
 #endif
 
       // Advance the second segment if it would still overlap the first
-      // 
+      //
       // The condition below is equivalent to
-      //   idx.second < Ny - 1 && intervals_overlap(idx.first, idx.second + 1)
-      //
-      // For the right-hand condition, we know that (by precondition)
-      //   get_loc1(idx.first) < get_loc2(idx.second + 1)
-      //
+      //   idx.second < N2 - 1 && intervals_overlap(idx.first, idx.second + 1)
+      // given that we know (by the precondition) that
+      //   loc1_left(idx.first) < loc2_right(idx.second)
       // and therefore that
-      //   get_loc1(idx.first) < get_loc2(idx.second + 2),
+      //   loc1_left(idx.first) < loc2_right(idx.second + 1),
       //
-      // so this inequality is all that remains to be checked.
-      //
-      if (idx.second < Ny - 1
-          && get_loc2(idx.second + 1) < get_loc1(idx.first + 1)) {
+      if (idx.second < segs.N2 - 1
+          && segs.loc2_left(idx.second + 1) < segs.loc1_right(idx.first)) {
         idx.second++;
       }
       // Could not advance the second segment above: advance the first instead,
       // and the second as well if they share an endpoint
       else {
-        if (idx.second < Ny - 1
-            && get_loc1(idx.first + 1) == get_loc2(idx.second + 1)) {
+        if (idx.second < segs.N2 - 1
+            && segs.loc2_left(idx.second + 1) == segs.loc1_right(idx.first)) {
           idx.second++;
         }
         idx.first++;
@@ -178,7 +179,7 @@ public:
 
 #if !NDEBUG
       // Verify postcondition
-      if (!(at_end() || intervals_overlap(idx.first, idx.second))) {
+      if (!(at_end() || segs.intervals_overlap(idx.first, idx.second))) {
         throw std::logic_error("Iterator postcondition not satisfied: "
                                "current intervals do not overlap (not at end)");
       }
@@ -193,6 +194,10 @@ public:
       return res;
     }
 
+    value_type operator*() const { return idx; }
+
+    const value_type *operator->() const { return &idx; }
+
     friend bool operator==(const iterator& lhs, const iterator& rhs) {
       return lhs.idx == rhs.idx;
     }
@@ -201,33 +206,25 @@ public:
       return !(lhs == rhs);
     }
   };
-
-  iterator begin() { return iterator(this); };
-  iterator end() { return iterator(this).advance_to_end(); };
 };
 
 
 double bowtie_area(double length, double val1_start, double val1_end,
-                          double val2_start, double val2_end);
+                   double val2_start, double val2_end);
+
 double get_segment(double start, double end, double val1_start,
-                          double val1_end, double val2_start, double val2_end);
+                   double val1_end, double val2_start, double val2_end);
+
 double get_segment_constrained(double seg1L1, double seg1L2,
-                                      double seg2L1, double seg2L2,
-                                      double seg1V1, double seg1V2,
-                                      double seg2V1, double seg2V2);
+                               double seg2L1, double seg2L2,
+                               double seg1V1, double seg1V2,
+                               double seg2V1, double seg2V2);
 
 double get_double_segment_constrained(
   double seg1Loc1, double seg1Loc2, double seg1Loc3,
   double seg1Val1, double seg1Val2,
   double seg2Loc1, double seg2Loc2, double seg2Loc3,
   double seg2Val1, double seg2Val2);
-
-inline double leftmost_segments(const NumericVector& loc1,
-                                const NumericVector& loc2,
-                                const NumericVector& val1,
-                                const NumericVector& val2,
-                                double binWidth1,
-                                double maxLoc);
 
 double NetEmdSmoothV2(NumericVector loc1, NumericVector val1, double binWidth1,
                       NumericVector loc2, NumericVector val2, double binWidth2);
